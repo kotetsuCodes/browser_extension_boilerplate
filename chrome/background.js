0eaaf8b83558d9f3958786c8a539965b74cd443c
Carl Brown <carllbrown87@gmail.com>
2017-02-26 12:59:57 -0600

    Finished out chrome extension

--- chrome/background.js
+++ chrome/background.js
@@ -1,34 +1,19 @@
 /**
  * Created by carl on 2/25/2017.
  */
-console.log('background page loaded...');
 
-var browser = (typeof(browser) == 'undefined') ? chrome : browser;
+try {
+    var browser = (typeof(browser) == 'undefined') ? chrome : browser;
 
-function completedRequestCallback(details) {
-    console.log('completed request callback running...');
-    browser.tabs.sendMessage(details.tabId, {requestDetails: details}, function (response) {
+    browser.tabs.onUpdated.addListener(function (tabId, info) {
+        if (info.status == "complete") {
+            var randomNumber = Math.floor((Math.random() * 999999) + 1);
+            browser.tabs.sendMessage(tabId, {message: 'Message from the background script! ' + randomNumber}, function (response) {
+            });
+        }
     });
-
+} catch (error) {
+    console.error(error);
 }
 
-browser.webRequest.onCompleted.addListener(completedRequestCallback, {urls: ['<all_urls>']});
-
-browser.tabs.onUpdated.addListener(function (tabId, info) {
-    console.log(tabId);
-    if (info.status == "complete") {
-        // your code ...
-
-        console.log(browser.webRequest.onCompleted.hasListener(completedRequestCallback));
-    }
-});
-
-
-// try {
-//     browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//         console.log(request);
-//     });
-// } catch (error) {
-//     console.log(error);
-// }
 
